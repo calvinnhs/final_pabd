d93f68e8a00b8b26dfbccc3b91574a79f36a6f5e
calvinnhs <tugasakun22@gmail.com>
2023-07-10 00:35:01 +0700

    MERUBAH NAMA TOMBOL

--- final_pabd/Form_DataDiri.Designer.cs
+++ final_pabd/Form_DataDiri.Designer.cs
@@ -111,7 +111,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "SAVE";
+            this.button1.Text = "INSERT";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -120,7 +120,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
-            this.button2.Text = "CLEAR";
+            this.button2.Text = "UPDATE";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -129,7 +129,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
-            this.button3.Text = "ADD";
+            this.button3.Text = "DELETE";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form_DataDiri
